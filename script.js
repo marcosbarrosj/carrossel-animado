1da52317ffae0d8a341ab3608ffcea9190b24219
marcos <166825415+marcosbarrosj@users.noreply.github.com>
2024-06-17 20:14:26 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+let btnNext = document.querySelector('.next')
+let btnback = document.querySelector('.back')
+
+let container = document.querySelector('.container')
+let list = document.querySelector('.container .list')
+let thumb = document.querySelector('.container .thumb')
+
+btnNext.onclick = () => moveItensOnclick('next')
+btnback.onclick = () => moveItensOnclick('back')
+
+
+    function moveItensOnclick(type){
+       let listItems = document.querySelectorAll('.list .list-item')
+       let thumbItems = document.querySelectorAll('.thumb .thumb-item')
+ console.log (listItems)
+ console.log (thumbItems)
+
+       
+       if (type === 'next'){
+           list.appendChild(listItems[0])
+           thumb.appendChild(thumbItems[0])
+        }else {
+            list.prepend(listItems[listItems.length -1])
+            thumb.prepend(thumbItems[listItems.length -1])
+
+            //   thumb.prepend(thumbItems[3])//
+
+
+
+        }
+
+
+    }
+
+
+
